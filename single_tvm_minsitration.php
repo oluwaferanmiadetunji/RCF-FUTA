01f258f347fe75f29b4a3a55252b604e4d840646
Oluwaferanmi <oluwaferanmiadetunji@gmail.com>
2020-04-21 20:19:04 +0100

    First commit

---
+++ single_tvm_minsitration.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>RCF FUTA</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="css/custom.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <?php include('sidebar.php')?>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <?php include('topbar.php')?>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <h1 class="h3 mb-4 text-gray-800">The True Vine Ministers &nbsp;<i class="fa fa-music"></i>&nbsp;<i class="fa fa-music"></i>&nbsp;<i class="fa fa-music"></i></h1>
+
+          <div class="row">
+
+            <div class="col-lg-6">
+              <!-- Circle Buttons -->
+              <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                  <h6 class="m-0 font-weight-bold text-primary">Title of Song</h6>
+                </div>
+                <div class="card" style="height:300px;overflow:auto;">
+                  <div class="card-body">
+                    <p class="card-text">
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br> 
+                      Vel iusto sint repellat cum illo commodi beatae. <br>
+                      Perspiciatis aperiam blanditiis in, eaque quod ratione,<br> 
+                      autem vel nostrum, earum saepe praesentium omnis!.<br>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br> 
+                      Vel iusto sint repellat cum illo commodi beatae. <br>
+                      Perspiciatis aperiam blanditiis in, eaque quod ratione,<br> 
+                      autem vel nostrum, earum saepe praesentium omnis!.<br>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br> 
+                      Vel iusto sint repellat cum illo commodi beatae. <br>
+                      Perspiciatis aperiam blanditiis in, eaque quod ratione,<br> 
+                      autem vel nostrum, earum saepe praesentium omnis!.<br>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br> 
+                      Vel iusto sint repellat cum illo commodi beatae. <br>
+                      Perspiciatis aperiam blanditiis in, eaque quod ratione,<br> 
+                      autem vel nostrum, earum saepe praesentium omnis!.<br>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br> 
+                      Vel iusto sint repellat cum illo commodi beatae. <br>
+                      Perspiciatis aperiam blanditiis in, eaque quod ratione,<br> 
+                      autem vel nostrum, earum saepe praesentium omnis!.<br>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br> 
+                      Vel iusto sint repellat cum illo commodi beatae. <br>
+                      Perspiciatis aperiam blanditiis in, eaque quod ratione,<br> 
+                      autem vel nostrum, earum saepe praesentium omnis!.<br>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br> 
+                      Vel iusto sint repellat cum illo commodi beatae. <br>
+                      Perspiciatis aperiam blanditiis in, eaque quod ratione,<br> 
+                      autem vel nostrum, earum saepe praesentium omnis!.<br>
+                      Lorem, ipsum dolor sit amet consectetur adipisicing elit.<br> 
+                      Vel iusto sint repellat cum illo commodi beatae. <br>
+                      Perspiciatis aperiam blanditiis in, eaque quod ratione,<br> 
+                      autem vel nostrum, earum saepe praesentium omnis!.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+
+      <?php include('footer.php')?>
+
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+ <?php include('logout.modal.php')?>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
